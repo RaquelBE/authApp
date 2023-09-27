5ee0862b77dfc870990f5f4c291c9c33d9eee513
Raquel Blazquez <raquel.blazquez@bosonit.com>
2023-09-27 16:11:13 +0200

    manejo de errores

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   templateUrl: './login-page.component.html',
@@ -7,13 +9,22 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginPageComponent {
   private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
 
   public myForm: FormGroup = this.fb.group({
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(6)]],
+    email: ['raquel@google.com', [Validators.required, Validators.email]],
+    password: ['123456', [Validators.required, Validators.minLength(6)]],
   });
 
   login() {
-    console.log(this.myForm.value);
+    const { email, password } = this.myForm.value;
+    
+    this.authService.login(email, password)
+      .subscribe({
+        next: () => console.log('Todo bien!'),
+        error: (message) => {
+          Swal.fire('Error', message, 'error')
+        }
+      } )
   }
 }
