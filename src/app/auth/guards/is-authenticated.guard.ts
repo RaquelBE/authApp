0f28e013f61194f746c3d610405ce82821d6e6ed
Raquel Blazquez <raquel.blazquez@bosonit.com>
2023-09-28 10:23:59 +0200

    implementar lógica del functional guard

--- src/app/auth/guards/is-authenticated.guard.ts
+++ src/app/auth/guards/is-authenticated.guard.ts
@@ -1,8 +1,17 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { inject } from '@angular/core';
+import { AuthStatus } from '../interfaces';
 
 export const isAuthenticatedGuard: CanActivateFn = (route, state) => {
+  //const url = state.url;
+  //localStorage.setItem('url', url);
+  const authService = inject(AuthService);
+  const router = inject(Router);
 
-  console.log('isAuthenticatedGuard')
-  console.log({route, state});
-  return true;
+  if (authService.authStatus() === AuthStatus.authenticated) {
+    return true;
+  }
+  router.navigateByUrl('/auth/login')
+  return false;
 };
