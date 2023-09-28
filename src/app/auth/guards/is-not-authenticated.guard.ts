2d61de036833ee518522abf773091be57e30afa9
Raquel Blazquez <raquel.blazquez@bosonit.com>
2023-09-28 12:26:45 +0200

    isnotauthenticated guard

--- src/app/auth/guards/is-not-authenticated.guard.ts
+++ src/app/auth/guards/is-not-authenticated.guard.ts
@@ -3,19 +3,16 @@ import { AuthService } from '../services/auth.service';
 import { inject } from '@angular/core';
 import { AuthStatus } from '../interfaces';
 
-export const isAuthenticatedGuard: CanActivateFn = (route, state) => {
+export const isNotAuthenticatedGuard: CanActivateFn = (route, state) => {
   //const url = state.url;
   //localStorage.setItem('url', url);
   const authService = inject(AuthService);
   const router = inject(Router);
 
   if (authService.authStatus() === AuthStatus.authenticated) {
-    return true;
-  }
-
-  if (authService.authStatus() === AuthStatus.checking) {
+    router.navigateByUrl('/dashboard');
     return false;
   }
-  router.navigateByUrl('/auth/login');
-  return false;
+
+  return true;
 };
