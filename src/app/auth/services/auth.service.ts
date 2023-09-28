2b0ec3f0a173440db4d44becda00288f7340c71c
Raquel Blazquez <raquel.blazquez@bosonit.com>
2023-09-28 11:58:22 +0200

    hacer que no se repita código

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -26,24 +26,22 @@ export class AuthService {
 
   constructor() {}
 
+  private setAuthentication(user: User, token: string): boolean {
+    this._currentUser.set(user);
+    this._authStatus.set(AuthStatus.authenticated);
+    localStorage.setItem('token', token);
+    return true;
+  }
+
   login(email: string, password: string): Observable<boolean> {
     const url = `${this.baseUrl}/auth/login`;
     const body = { email, password };
 
     return this.http.post<LoginResponse>(url, body).pipe(
-      tap(({ user, token }) => {
-        this._currentUser.set(user);
-        this._authStatus.set(AuthStatus.authenticated);
-        localStorage.setItem('token', token);
-        console.log({ user, token });
-      }),
-      map(() => true),
+      map(({ user, token }) => this.setAuthentication(user, token)),
       //errores
       catchError((err) => throwError(() => err.error.message))
     );
-  }
-  setAuthentication(user: User, token: string): any {
-    throw new Error('Method not implemented.');
   }
 
   checkAuthStatus(): Observable<boolean> {
@@ -55,12 +53,7 @@ export class AuthService {
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
     return this.http.get<CheckTokenResponse>(url, { headers }).pipe(
-      map(({ token, user }) => {
-        this._currentUser.set(user);
-        this._authStatus.set(AuthStatus.authenticated);
-        localStorage.setItem('token', token);
-        return true;
-      }),
+      map(({ user, token }) => this.setAuthentication(user, token)),
       catchError(() => {
         this._authStatus.set(AuthStatus.notAuthenticated);
         return of(false);
